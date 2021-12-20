ed9e7648a74d63fc182bd12bef247ea8fe46ab23
unknown <abdurrahmanseyidoglu@gmail.com>
2021-12-20 21:41:39 +0300

    rendering game board with random colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+
+let width = 8
+const candyColors = [
+    "blue",
+    "green",
+    "orange",
+    'purple',
+    "red",
+    'yellow'
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+    //creating an array of 8*8 = 64 square each one is a random color from the candyColors array
+    const createBoard = () => {
+        const randomColorArrangement = []
+        for (let i = 0; i < width * width; i++) {
+            const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+            randomColorArrangement.push(randomColor)
+        }
+        setCurrentColorArrangement(randomColorArrangement)
+    }
+    //using effect without dependencies to run the "createBoard()" function only once (when rendering is finished)
+    useEffect(() => {
+        createBoard()
+    }, [])
+
+    console.log(currentColorArrangement)
+    return (
+        <div className="app">
+            <div className="game">
+                {
+                    currentColorArrangement.map((candyColor, index) => {
+                        return (
+                            <img key={index} style={{backgroundColor: candyColor}} alt="a candy piece"/>
+                        )
+                    })
+                }
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
