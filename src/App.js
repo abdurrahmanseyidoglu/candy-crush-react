78a3e0a62364c153b7886fc4d5b5228876a3140a
unknown <abdurrahmanseyidoglu@gmail.com>
2021-12-21 13:44:45 +0300

    columns that have three or four similar color will get removed from the board

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import {useEffect, useState} from "react";
 
 let width = 8
-const candyColors = [
-    "blue",
-    "green",
-    "orange",
-    'purple',
-    "red",
-    'yellow'
-]
+const candyColors = ["blue", "green", "orange", 'purple', "red", 'yellow']
 
 function App() {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+    //check for columns of four of the same color
+    const checkForColumnOfFour = () => {
+        for (let i = 0; i < 36; i++) {
+            const columnOfFour = [i, i + width, i + (width * 2), i + (width * 3)]
+            const currentColor = currentColorArrangement[i]
+            if (columnOfFour.every(indexOfColor => currentColorArrangement[indexOfColor] === currentColor)) {
+                columnOfFour.forEach(indexOfColor => currentColorArrangement[indexOfColor] = '')
+            }
+        }
+    }
+    //check for columns of three of the same color
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i < 47; i++) {
+            const columnOfThree = [i, i + width, i + (width * 2)]
+            const currentColor = currentColorArrangement[i]
+            if (columnOfThree.every(indexOfColor => currentColorArrangement[indexOfColor] === currentColor)) {
+                columnOfThree.forEach(indexOfColor => currentColorArrangement[indexOfColor] = '')
+            }
+        }
+    }
+
 
     //creating an array of 8*8 = 64 square each one is a random color from the candyColors array
     const createBoard = () => {
@@ -27,21 +41,26 @@ function App() {
         createBoard()
     }, [])
 
-    console.log(currentColorArrangement)
-    return (
-        <div className="app">
-            <div className="game">
-                {
-                    currentColorArrangement.map((candyColor, index) => {
-                        return (
-                            <img key={index} style={{backgroundColor: candyColor}} alt="a candy piece"/>
-                        )
-                    })
-                }
+    //call checkForColumnOfThree when the component gets mount and keep calling it every 100 ms
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfFour()
+            checkForColumnOfThree()
 
-            </div>
+            setCurrentColorArrangement([...currentColorArrangement])
+        }, 100)
+        return () => clearInterval(timer)
+    }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+
+    console.log(currentColorArrangement)
+    return (<div className="app">
+        <div className="game">
+            {currentColorArrangement.map((candyColor, index) => {
+                return (<img key={index} style={{backgroundColor: candyColor}}/>)
+            })}
+
         </div>
-    );
+    </div>);
 }
 
 export default App;
